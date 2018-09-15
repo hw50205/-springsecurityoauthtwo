d72d6fa28d464955a053afd98e17515474c48315
hw50205 <1317201840@qq.com>
2018-09-15 19:07:26 +0800

    init

--- user-service/src/main/java/com/example/userservice/controller/TestController.java
+++ user-service/src/main/java/com/example/userservice/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.example.userservice.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.UUID;
 
 @RestController
@@ -12,7 +14,7 @@ public class TestController {
 
     @GetMapping
 //    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    public String getFoo() {
-        return "I'm foo, " + UUID.randomUUID().toString();
+    public String getFoo(Principal principal) {
+        return "I'm " + principal.getName() + ", " + UUID.randomUUID().toString();
     }
 }
